b76672b14ed9a9ddac0fbab5b93b89a0a94f0a35
Hugo Chatigny <hugochatigny@gmail.com>
2023-03-02 21:22:27 +0100

    home end

--- src/assets/js/menu.js
+++ src/assets/js/menu.js
@@ -7,3 +7,32 @@ btnMenu.addEventListener('click', ()=>{
     btnMenu.classList.toggle('main-menu--active')
 
 })
+
+function change() {
+ 
+    select = document.getElementById("select");
+    select_s = select.style;
+     
+    switch(select.selectedIndex) {
+     
+    case 0 :
+    select_s.background = "url('assets/img/mars.png') no-repeat";
+    break;
+     
+    case 1 :
+    select_s.background = "url('assets/img/planete.png') no-repeat";
+    break;
+     
+    case 2 :
+    select_s.background = "url('assets/img/mars.png') no-repeat";
+    break;
+     
+    case 3 :
+    select_s.background = "url('assets/img/mars.png') no-repeat";
+    break;
+     
+    default:
+    select_s.background = "none";
+    break;
+    }
+    }
